102c6ceedde7b063afd7e528d8ccfce4723f703b
David_Cech <david.cech@student.gyarab.cz>
2019-12-30 18:11:48 +0100

    Updated createDiaryEntry, now receives most of the data from json body, calculate summary was updated to the format of json it receives and schema DiaryEntry. Still some tweaks left.

--- routes/diaryEntries.js
+++ routes/diaryEntries.js
@@ -10,17 +10,42 @@ router.post('/create', passport.authenticate('jwt', { session: false }), (req, r
 
     var newEntry = new DiaryEntry();
 
-    let meals = req.body.meals;
-    for (let i = 0; i < 6; i++) {
-        newEntry.meals.push(meals[i]);
-    }
     newEntry.date = new Date(req.body.date);
-    newEntry.nutritionSummary = {kcal: 9, protein: 3, fibre: 2, fat: 1, carbs: 4};
     newEntry.author = req.user._id;
-    newEntry.activities = req.body.activities;
-    newEntry.nutritionSummary = calculateSummary(meals, req.body.activities);
+    if(req.body.activities.kcal !== ""){
+        newEntry.activities = {...newEntry.activities, kcal: req.body.activities.kcal};
+    }
+    if(req.body.activities.description !== ""){
+        newEntry.activities = {...newEntry.activities, description: req.body.activities.description};
+    }
 
-    res.json(newEntry);
+    // console.log(Object.entries(req.body.meals));
+
+    // let meals = req.body.meals;
+    // for (let i = 0; i < 6; i++) {
+    //     newEntry.meals.push(meals[i]);
+    // }
+
+    let mealNames = ["breakfast", "morning_snack", "lunch", "afternoon_snack", "dinner", "other"];
+    let meal = {kcal: 0, protein: 0, fibre: 0, fat: 0, carbs: 0, contents: [], name: ""};
+    let mealEntry = Object.entries(meal);
+    let summaryEntry;
+    //Calculates nutritionSummary for every meal additionally assigns name to it and creates its content
+    for(let i = 0; i<mealNames.length;i++){
+        mealEntry[6] = mealNames[i];
+        summaryEntry = calculateSummary(Object.entries(req.body.meals)[i][1], {kcal:0});
+        for (let j = 0; j < summaryEntry.length; j++){
+            mealEntry[j] = summaryEntry[j];
+        }
+        //Most likely newEntry.meals.push(Object.fromEntries(mealEntry));
+        console.log(mealEntry)
+    }
+
+    // newEntry.meals.push();
+    // calculateSummary(Object.entries(req.body.meals)[1][1], newEntry.activities);
+    //
+    // console.log(req.body, newEntry);
+    res.json(req.body);
     /*newEntry.save(function (err, entry) {
         if (err) {
              res.send(err);
@@ -31,19 +56,27 @@ router.post('/create', passport.authenticate('jwt', { session: false }), (req, r
 });
 
 calculateSummary = (meals, activites) =>{
-    let nutritionSummary = {kcal: 0, protein: 0, fibre: 0, fat: 0, carbs: 0};
+    let nutritionSummary = {kcal: 0, protein: 0, fibre: 0, fat: 0, carbs: 0, contents: []};
 
-    let entries = Object.entries(nutritionSummary);
+    let summaryEntries = Object.entries(nutritionSummary);
     for (let j = 0; j < meals.length; j++) {
-        let mealEntry = Object.entries(meals[j]);
-        for (let k = 0; k < entries.length; k++) {
-                entries[k][1] += mealEntry[k][1];
+        let mealEntry = Object.entries((meals[j]).food.nutritionVal);
+        let grams = meals[j].grams;
+        if(parseInt(meals[j].grams)===1){
+            summaryEntries[5][1].push(meals[j].food.name + " " + meals[j].grams + " gram");
+        } else if(meals[j].grams<5){
+            summaryEntries[5][1].push(meals[j].food.name + " " + meals[j].grams + " gramy");
+        } else {
+            summaryEntries[5][1].push(meals[j].food.name + " " + meals[j].grams + " gramu");
+        }
+        for (let k = 0; k < summaryEntries.length-1; k++) {
+                summaryEntries[k][1] += Math.round(100 *(mealEntry[k][1]*(grams/100))/100);
         }
     }
 
-    entries[0][1] -= activites.kcal;
+    summaryEntries[0][1] -= activites.kcal;
 
-    return Object.fromEntries(entries);
+    return summaryEntries;
 };
 
 module.exports = router;
